cf75c53df11c92b1de06e29f1ccdd4f3c14ba95c
gateremark <mark5gatere@gmail.com>
2024-01-03 01:20:00 +0300

    refactor: add blog data in separate file

---
+++ app/components/BlogData.tsx
@@ -0,0 +1,50 @@
+export const blogData = [
+    {
+        title: "Blog 1",
+        description: "Blog 1 description",
+        date: "2021-07-01",
+        slug: "blog-1",
+    },
+    {
+        title: "Blog 2",
+        description: "Blog 2 description",
+        date: "2021-07-02",
+        slug: "blog-2",
+    },
+    {
+        title: "Blog 3",
+        description: "Blog 3 description",
+        date: "2021-07-03",
+        slug: "blog-3",
+    },
+    {
+        title: "Blog 4",
+        description: "Blog 4 description",
+        date: "2021-07-04",
+        slug: "blog-4",
+    },
+    {
+        title: "Blog 5",
+        description: "Blog 5 description",
+        date: "2021-07-05",
+        slug: "blog-5",
+    },
+    {
+        title: "Blog 6",
+        description: "Blog 6 description",
+        date: "2021-07-06",
+        slug: "blog-6",
+    },
+    {
+        title: "Blog 7",
+        description: "Blog 7 description",
+        date: "2021-07-07",
+        slug: "blog-7",
+    },
+    {
+        title: "Blog 8",
+        description: "Blog 8 description",
+        date: "2021-07-08",
+        slug: "blog-8",
+    },
+];
